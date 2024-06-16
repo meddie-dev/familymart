fa430d988f6682cdfb979568f34a8628dc302567
meddie-dev <johnmeddiemariano@gmail.com>
2024-06-16 21:36:49 +0800

    redo update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React, { useState, useEffect } from 'react'
+import Landing from './pages/landing_page.jsx'
+import Home from './pages/home.jsx'
+import Shop from './pages/shop.jsx'
+import Create from './pages/create_acc.jsx'
+import Signin from './pages/sign_in.jsx'
+import Account from './pages/account.jsx'
+import ShoppingCart from './pages/shopping_cart.jsx'
+import Company from './pages/company.jsx'
+import MarketPlace from './pages/market_place.jsx'
+import Checkout from './pages/checkout.jsx'
 import './index.css'
-import { BrowserRouter } from "react-router-dom"; 
-import Main from './Main.jsx'
 
+import { Route, Routes } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
+ function App() {
+  const [loading, setLoading] = useState(false)
 
-  <React.StrictMode>
-    <BrowserRouter>
-      <Main />
-    </BrowserRouter>
-  </React.StrictMode>,
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+  }, [])
 
-)
+  return (
+    <>{
+      loading ? 
+        <div className="w-full h-screen flex justify-center items-center overflow-y-hidden">
+          <video className="w-full h-full object-cover" src="src/assets/Untitled design.mp4" autoPlay muted playsInline loop loading={loading}></video>
+        </div>
+      :
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/sign_in" element={<Signin />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/shopping_cart" element={<ShoppingCart />} />
+        <Route path="/company" element={<Company />} />
+        <Route path="/market_place" element={<MarketPlace />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+      
+    }
+    </>
+  )
+}
 
-
+export default App
