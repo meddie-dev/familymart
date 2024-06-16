adbf3cdba441aabb7c68ca56c4205cf6d45ce344
meddie-dev <johnmeddiemariano@gmail.com>
2024-06-16 21:26:03 +0800

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { BrowserRouter } from "react-router-dom"; 
+import App from './App.jsx'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+
+)
+
 import React, { useState, useEffect } from 'react'
 import Landing from './pages/landing_page.jsx'
 import Home from './pages/home.jsx'
@@ -10,41 +28,3 @@ import Company from './pages/company.jsx'
 import MarketPlace from './pages/market_place.jsx'
 import Checkout from './pages/checkout.jsx'
 import './index.css'
-
-import { Route, Routes } from 'react-router-dom'
-
- function App() {
-  const [loading, setLoading] = useState(false)
-
-  useEffect(() => {
-    setLoading(true)
-    setTimeout(() => {
-      setLoading(false)
-    }, 3000)
-  }, [])
-
-  return (
-    <>{
-      loading ? 
-        <div className="w-full h-screen flex justify-center items-center overflow-y-hidden">
-          <video className="w-full h-full object-cover" src="src/assets/Untitled design.mp4" autoPlay muted playsInline loop loading={loading}></video>
-        </div>
-      :
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/create" element={<Create />} />
-        <Route path="/sign_in" element={<Signin />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/shopping_cart" element={<ShoppingCart />} />
-        <Route path="/company" element={<Company />} />
-        <Route path="/market_place" element={<MarketPlace />} />
-        <Route path="/checkout" element={<Checkout />} />
-      </Routes>
-      
-    }
-    </>
-  )
-}
-
-export default App
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,17 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { BrowserRouter } from "react-router-dom"; 
-import App from './App.jsx'
 
+import { Route, Routes } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
+ function App() {
+  const [loading, setLoading] = useState(false)
 
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+  }, [])
 
-)
+  return (
+    <>{
+      loading ? 
+        <div className="w-full h-screen flex justify-center items-center overflow-y-hidden">
+          <video className="w-full h-full object-cover" src="src/assets/Untitled design.mp4" autoPlay muted playsInline loop loading={loading}></video>
+        </div>
+      :
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/sign_in" element={<Signin />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/shopping_cart" element={<ShoppingCart />} />
+        <Route path="/company" element={<Company />} />
+        <Route path="/market_place" element={<MarketPlace />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+      
+    }
+    </>
+  )
+}
+
+export default App
