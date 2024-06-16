de3bb670529c23c42e365092c30253653fa2f614
meddie-dev <johnmeddiemariano@gmail.com>
2024-06-17 06:08:53 +0800

    Main Updated

--- src/components/promo_section_overlap.jsx
+++ src/components/promo_section_overlap.jsx
@@ -11,7 +11,7 @@ export default function PromoSectionOverlap() {
                     <div className="relative max-w-[90%] mx-auto mb-20 flex flex-row gap-9  max-lg:flex-row max-lg:max-w-[97%] max-lg:mx-auto  max-md:flex-col max-md:max-w-[75%]  max-sm:flex-col max-sm:max-w-[75%]">
                         
                         <div className="relative flex drop-shadow-xl" >
-                            <img className="rounded-xl brightness-[90%] " src={import.meta.env.BASE_URL + "src/assets/category/pm1.jpg"} alt="Prescription Medicine image" />
+                            <img className="rounded-xl brightness-[90%] " src={import.meta.env.BASE_URL + "./assets/category/pm1.jpg"} alt="Prescription Medicine image" />
                             <div className="absolute bottom-7 left-7 flex flex-col z-10">
                                 <h3 className="text-sm font-semibold text-white my-1">Shop the Collection</h3>
                                 <p className="text-md font-bold text-white">Prescription Medicine</p>
@@ -19,7 +19,7 @@ export default function PromoSectionOverlap() {
                             <div className="absolute rounded-lg top-0 right-0 opacity-30 w-full h-full bg-gradient-to-t from-gray-500 "></div>
                         </div>
                         <div className="relative flex drop-shadow-xl" >
-                            <img className="rounded-xl brightness-[90%]" src={import.meta.env.BASE_URL + "src/assets/category/pm2.avif"} alt="Baby & Kids image" />
+                            <img className="rounded-xl brightness-[90%]" src={import.meta.env.BASE_URL + "./assets/category/pm2.avif"} alt="Baby & Kids image" />
                             <div className="absolute bottom-7 left-7 flex flex-col z-10">
                                 <h3 className="text-sm font-semibold text-white my-1">Shop the Collection</h3>
                                 <p className="text-md font-bold text-white"> Baby & Kids </p>
@@ -27,7 +27,7 @@ export default function PromoSectionOverlap() {
                             <div className="absolute rounded-lg top-0 right-0 opacity-30 w-full h-full bg-gradient-to-t from-gray-500 "></div>
                         </div>
                         <div className="relative flex drop-shadow-xl" >
-                            <img className="rounded-xl brightness-[90%]" src={import.meta.env.BASE_URL + "src/assets/category/pm3.jpg"} alt="Personal care image" />
+                            <img className="rounded-xl brightness-[90%]" src={import.meta.env.BASE_URL + "./assets/category/pm3.jpg"} alt="Personal care image" />
                             <div className="absolute bottom-7 left-7 flex flex-col z-10">
                                 <h3 className="text-sm font-semibold text-white my-1">Shop the Collection</h3>
                                 <p className="text-md font-bold text-white">Personal care</p>
--- src/layouts/category.jsx
+++ src/layouts/category.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import pm3 from "./assets/category/pm3.jpg"
+import pm1 from "./assets/category/pm1.jpg" 
 
 export default function Category() {
     return (
@@ -20,7 +22,7 @@ export default function Category() {
                 <div className="max-w-[90%] mx-auto">
                     <div className="grid grid-cols-2 gap-8  max-md:max-w-[90%]  max-md:mx-auto max-md:gap-5 max-sm:flex max-sm:flex-col ">
                         <div className="relative max-md:h-[30rem] max-sm:max-h-[14rem] ">
-                            <img className="object-cover h-full w-full  rounded-md brightness-[80%] " src= {import.meta.env.BASE_URL + "./assets/category/pm1.jpg"} alt="Product image" />
+                            <img className="object-cover h-full w-full  rounded-md brightness-[80%] " src= {pm1} alt="Product image" />
                             <div className="absolute bottom-0 left-0 text-white m-3 p-3 ">
                                 <h3 className="font-bold">New Arrivals</h3>
                                 <p>Prescription Medicine</p>
@@ -36,7 +38,7 @@ export default function Category() {
                                 </div>
                             </div>
                             <div className="relative h-80 max-md:max-h-[14rem]  ">
-                                <img className="object-cover h-[20rem] max-md:h-[14rem]  w-full rounded-md brightness-[80%] " src= {import.meta.env.BASE_URL + "./assets/category/pm3.jpg"} alt="Product image" />
+                                <img className="object-cover h-[20rem] max-md:h-[14rem]  w-full rounded-md brightness-[80%] " src={pm3} alt="Product image" />
                                 <div className="absolute bottom-0 left-0  text-white m-3 p-3 ">
                                     <h3 className="font-bold">New Arrivals</h3>
                                     <p>Personal care</p>
