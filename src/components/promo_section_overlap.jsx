549fbdceb18591f0fdea666f2a4606c2a46979e6
meddie-dev <johnmeddiemariano@gmail.com>
2024-06-17 07:36:12 +0800

    promo_section_overlap Update

--- src/components/promo_section_overlap.jsx
+++ src/components/promo_section_overlap.jsx
@@ -1,3 +1,7 @@
+import pm1 from '../assets/category/pm1.jpg'
+import pm2 from '../assets/category/pm2.avif'
+import pm3 from '../assets/category/pm3.avif'
+import bg3 from '../assets/bg3.jpg'
 export default function PromoSectionOverlap() {
     return (
         <>
@@ -11,7 +15,7 @@ export default function PromoSectionOverlap() {
                     <div className="relative max-w-[90%] mx-auto mb-20 flex flex-row gap-9  max-lg:flex-row max-lg:max-w-[97%] max-lg:mx-auto  max-md:flex-col max-md:max-w-[75%]  max-sm:flex-col max-sm:max-w-[75%]">
                         
                         <div className="relative flex drop-shadow-xl" >
-                            <img className="rounded-xl brightness-[90%] " src={import.meta.env.BASE_URL + "./assets/category/pm1.jpg"} alt="Prescription Medicine image" />
+                            <img className="rounded-xl brightness-[90%] " src={pm1} alt="Prescription Medicine image" />
                             <div className="absolute bottom-7 left-7 flex flex-col z-10">
                                 <h3 className="text-sm font-semibold text-white my-1">Shop the Collection</h3>
                                 <p className="text-md font-bold text-white">Prescription Medicine</p>
@@ -19,7 +23,7 @@ export default function PromoSectionOverlap() {
                             <div className="absolute rounded-lg top-0 right-0 opacity-30 w-full h-full bg-gradient-to-t from-gray-500 "></div>
                         </div>
                         <div className="relative flex drop-shadow-xl" >
-                            <img className="rounded-xl brightness-[90%]" src={import.meta.env.BASE_URL + "./assets/category/pm2.avif"} alt="Baby & Kids image" />
+                            <img className="rounded-xl brightness-[90%]" src={pm2} alt="Baby & Kids image" />
                             <div className="absolute bottom-7 left-7 flex flex-col z-10">
                                 <h3 className="text-sm font-semibold text-white my-1">Shop the Collection</h3>
                                 <p className="text-md font-bold text-white"> Baby & Kids </p>
@@ -27,7 +31,7 @@ export default function PromoSectionOverlap() {
                             <div className="absolute rounded-lg top-0 right-0 opacity-30 w-full h-full bg-gradient-to-t from-gray-500 "></div>
                         </div>
                         <div className="relative flex drop-shadow-xl" >
-                            <img className="rounded-xl brightness-[90%]" src={import.meta.env.BASE_URL + "./assets/category/pm3.jpg"} alt="Personal care image" />
+                            <img className="rounded-xl brightness-[90%]" src={pm3} alt="Personal care image" />
                             <div className="absolute bottom-7 left-7 flex flex-col z-10">
                                 <h3 className="text-sm font-semibold text-white my-1">Shop the Collection</h3>
                                 <p className="text-md font-bold text-white">Personal care</p>
@@ -38,7 +42,7 @@ export default function PromoSectionOverlap() {
                     </div>
                 </div>
                 <div className="absolute top-[-5rem] right-0 left-0 z-[-10] brightness-75 blur-sm">
-                    <img className=" object-cover h-[40rem] w-full max-xl:h-[36rem] max-lg:h-[31rem] max-md:h-[29rem] max-sm:h-[33rem]  " src={import.meta.env.BASE_URL + "./assets/bg3.jpg"} alt="bg3.jpg" />
+                    <img className=" object-cover h-[40rem] w-full max-xl:h-[36rem] max-lg:h-[31rem] max-md:h-[29rem] max-sm:h-[33rem]  " src={bg3} alt="bg3.jpg" />
                     <div className="absolute rounded-lg top-0 right-0 opacity-40 w-full h-full bg-gradient-to-b from-[#637eb9] "></div>
                 </div>
                 
