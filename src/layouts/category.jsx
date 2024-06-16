fc200ec2667a2ec566000acdb0c6f3c01b9cf1b0
meddie-dev <johnmeddiemariano@gmail.com>
2024-06-17 06:19:55 +0800

    Main Updated

--- src/layouts/category.jsx
+++ src/layouts/category.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import pm3 from "../assets/category/pm3.jpg"
-import pm1 from "../assets/category/pm1.jpg" 
 
 export default function Category() {
     return (
@@ -22,7 +20,7 @@ export default function Category() {
                 <div className="max-w-[90%] mx-auto">
                     <div className="grid grid-cols-2 gap-8  max-md:max-w-[90%]  max-md:mx-auto max-md:gap-5 max-sm:flex max-sm:flex-col ">
                         <div className="relative max-md:h-[30rem] max-sm:max-h-[14rem] ">
-                            <img className="object-cover h-full w-full  rounded-md brightness-[80%] " src= {pm1} alt="Product image" />
+                            <img className="object-cover h-full w-full  rounded-md brightness-[80%] " src= {import.meta.env.BASE_URL + "src/assets/category/pm1.jpg"} alt="Product image" />
                             <div className="absolute bottom-0 left-0 text-white m-3 p-3 ">
                                 <h3 className="font-bold">New Arrivals</h3>
                                 <p>Prescription Medicine</p>
@@ -31,14 +29,14 @@ export default function Category() {
 
                         <div className="flex flex-col gap-5 justify-between max-md:justify-between  ">
                             <div className="relative h-80 max-md:max-h-[14rem]  ">
-                                <img className="object-cover h-[20rem] max-md:h-[14rem]  w-full rounded-md brightness-[80%] " src= {import.meta.env.BASE_URL + "./assets/category/pm2.avif"} alt="Product image" />
+                                <img className="object-cover h-[20rem] max-md:h-[14rem]  w-full rounded-md brightness-[80%] " src= {import.meta.env.BASE_URL + "src/assets/category/pm2.avif"} alt="Product image" />
                                 <div className="absolute bottom-0 left-0  text-white m-3 p-3 ">
                                     <h3 className="font-bold">New Arrivals</h3>
                                     <p>Baby & Kids</p>
                                 </div>
                             </div>
                             <div className="relative h-80 max-md:max-h-[14rem]  ">
-                                <img className="object-cover h-[20rem] max-md:h-[14rem]  w-full rounded-md brightness-[80%] " src={pm3} alt="Product image" />
+                                <img className="object-cover h-[20rem] max-md:h-[14rem]  w-full rounded-md brightness-[80%] " src= "src/assets/category/pm3.jpg" alt="Product image" />
                                 <div className="absolute bottom-0 left-0  text-white m-3 p-3 ">
                                     <h3 className="font-bold">New Arrivals</h3>
                                     <p>Personal care</p>
