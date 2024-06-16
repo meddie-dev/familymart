cc102d55f4bcf33b60aaeebc1d3693cd667b25e5
meddie-dev <johnmeddiemariano@gmail.com>
2024-06-16 21:30:06 +0800

    app Updated

--- src/App.jsx
+++ src/App.jsx
@@ -16,15 +16,4 @@ root.render(
 
 )
 
-import React, { useState, useEffect } from 'react'
-import Landing from './pages/landing_page.jsx'
-import Home from './pages/home.jsx'
-import Shop from './pages/shop.jsx'
-import Create from './pages/create_acc.jsx'
-import Signin from './pages/sign_in.jsx'
-import Account from './pages/account.jsx'
-import ShoppingCart from './pages/shopping_cart.jsx'
-import Company from './pages/company.jsx'
-import MarketPlace from './pages/market_place.jsx'
-import Checkout from './pages/checkout.jsx'
-import './index.css'
+
